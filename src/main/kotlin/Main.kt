c209b51fda472e164acb7e2c0c6914edc99931c4
Olga Yurina <olga.nik.yurina@gmail.com>
2023-05-19 09:07:40 +0300

    Change result commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
 fun main() {
     val commission = 0.0075
     val minimumCommissionSum = 35_00
-    val amount = 10_000_00
-    val totalCommissionSum = amount*commission
+    val amount = 10_035_50
+    val totalCommissionSum = (amount*commission).toInt()
     val resultCommissionSum = if( totalCommissionSum < minimumCommissionSum) minimumCommissionSum else totalCommissionSum
-    val resultCommissionSumRub = resultCommissionSum.toDouble()/100
-    println(resultCommissionSumRub)
+    println("Комиссия за перевод " + resultCommissionSum/100 + "руб. " + resultCommissionSum%100 + " коп.")
 
 }
